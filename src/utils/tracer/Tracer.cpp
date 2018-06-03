10cb850a3200a27611b349217f901593e42edc06
kraftwerk28 <kefirchik3@gmail.com>
2018-06-03 13:58:02 +0300

    render | bmp creator | extended vector class

--- src/utils/tracer/Tracer.cpp
+++ src/utils/tracer/Tracer.cpp
@@ -2,12 +2,9 @@
 
 using namespace std;
 
-Vector::Vector()
-{
-  x = 0;
-  y = 0;
-  z = 0;
-}
+Camera main_camera;
+
+Vector::Vector() : x(0), y(0), z(0) {}
 
 Vector::Vector(const float coordinates[3])
 {
@@ -23,18 +20,77 @@ Vector::Vector(vector<float> vector)
   z = vector[2];
 }
 
-Vector::Vector(float X, float Y, float Z): x(X), y(Y), z(Z) {}
+Vector::Vector(float X, float Y, float Z) : x(X), y(Y), z(Z) {}
+
+Vector::operator bool()
+{
+  return intersects;
+}
+
+Vector Vector::operator+(Vector &v)
+{
+  return Vector(x + v.x, y + v.y, z + v.z);
+}
+Vector Vector::operator-(Vector &v)
+{
+  return Vector(x - v.x, y - v.y, z - v.z);
+}
+
+Vector Vector::operator*(float num)
+{
+  return Vector(x * num, y * num, z * num);
+}
+
+bool Vector::operator==(Vector &v)
+{
+  return (x == v.x) && (y == v.y) && (z == v.z);
+}
+
+float Vector::dot(Vector &v)
+{
+  return x * v.x + y * v.y + z * v.z;
+}
+
+Vector Vector::cross(Vector &v)
+{
+  return Vector(
+      y * v.z - z * v.y,
+      z * v.x - x * v.z,
+      x * v.y - y * v.x);
+}
+
+Vector Vector::normalize()
+{
+  float l = sqrtf(x * x + y * y + z * z);
+  return *new Vector(x / l, y / l, z / l);
+}
+
+float Vector::length()
+{
+  return sqrtf(x * x + y * y + z * z);
+}
+
+void Vector::print()
+{
+  cout << "(" << x << ", " << y << ", " << z << ")\n";
+}
 
 Vertex::Vertex(Vector vec)
 {
   pt = vec;
 }
 
-Facet::Facet(Vector v1, Vector v2, Vector v3)
+Facet::Facet(Vector &v1, Vector &v2, Vector &v3)
 {
   v[0] = v1;
   v[1] = v2;
   v[2] = v3;
+}
+
+Ray::Ray(Vector _origin, Vector _direction)
+{
+  origin = _origin;
+  direction = _direction;
 }
 
 void Tracer::parseOBJ(const char *fname, vector<Vertex> &vertices, vector<Facet> &faces)
@@ -55,16 +111,19 @@ void Tracer::parseOBJ(const char *fname, vector<Vertex> &vertices, vector<Facet>
       {
       case 'v':
       {
-        vector<string> coords = strsplit<string>(str, ' ');
-        vertices.push_back(Vertex(Vector(stof(coords[1]), stof(coords[2]), stof(coords[3]))));
+        if (str[1] == ' ')
+        {
+          vector<string> coords = strsplit<string>(str, ' ');
+          vertices.push_back(Vertex(Vector(stof(coords[1]), stof(coords[2]), stof(coords[3]))));
+        }
         break;
       }
       case 'f':
       {
         vector<string> pts = strsplit<string>(str, ' ');
-        int v1 = stoi(strsplit<string>(pts[1], '/')[0]),
-                v2 = stoi(strsplit<string>(pts[2], '/')[0]),
-                v3 = stoi(strsplit<string>(pts[3], '/')[0]);
+        int v1 = stoi(strsplit<string>(pts[1], '/')[0]) - 1,
+            v2 = stoi(strsplit<string>(pts[2], '/')[0]) - 1,
+            v3 = stoi(strsplit<string>(pts[3], '/')[0]) - 1;
         Facet f(vertices[v1].pt, vertices[v2].pt, vertices[v3].pt);
         faces.push_back(f);
         vertices[v1].facets.push_back(f);
@@ -81,23 +140,79 @@ void Tracer::parseOBJ(const char *fname, vector<Vertex> &vertices, vector<Facet>
   {
     log("Error opening " + (string)fname);
   }
+  cout << "Vertex count: " << vertices.size() << "\n Faces count: " << faces.size() << "\n";
 }
 
-// Vector Tracer::local_to_world(Vector *child, Vector *parent)
-// {
-//   const float c[3] = {child->x + parent->x, child->y + parent->y, child->z + parent->z};
-//   return Vector(c);
-// }
-// Vector Tracer::world_to_local(Vector *child, Vector *parent)
-// {
-//   const float c[3] = {child->x - parent->x, child->y - parent->y, child->z - parent->z};
-//   return Vector(c);
-// }
+Vector Tracer::intersectsTriangle(Facet &facet, Ray &ray)
+{
+  const float E = 1e-5;
+  float
+      x0 = facet.v[0].x,
+      y0 = facet.v[0].y,
+      z0 = facet.v[0].z,
+      x1 = facet.v[1].x,
+      y1 = facet.v[1].y,
+      z1 = facet.v[1].z,
+      x2 = facet.v[2].x,
+      y2 = facet.v[2].y,
+      z2 = facet.v[2].z,
+
+      A = (y1 - y0) * (z2 - z0) - (y2 - y0) * (z1 - z0),
+      B = (x2 - x0) * (z1 - z0) - (x1 - x0) * (z2 - z0),
+      C = (x1 - x0) * (y2 - y0) - (y1 - y0) * (x2 - x0),
+      D =
+          x0 * ((y2 - y0) * (z1 - z0) - (y1 - y0) * (z2 - z0)) +
+          y0 * ((x1 - x0) * (z2 - z0) - (x2 - x0) * (z1 - z0)) +
+          z0 * ((y1 - y0) * (x2 - x0) - (x1 - x0) * (y2 - y0)),
+
+      t =
+          -(A * ray.origin.x + B * ray.origin.y + C * ray.origin.z + D) /
+          (A * ray.direction.x + B * ray.direction.y + C * ray.direction.z),
+
+      x = t * ray.direction.x + ray.origin.x,
+      y = t * ray.direction.y + ray.origin.y,
+      z = t * ray.direction.z + ray.origin.z;
+
+  Vector res(x, y, z);
+  Vector edge1 = facet.v[1] - facet.v[0],
+         edge2 = facet.v[2] - facet.v[0],
+         h = ray.direction.cross(edge2);
+  float a = edge1.dot(h);
+  if (a > -E && a < E)
+    return res;
+
+  float f = 1 / a;
+  Vector s = ray.origin - facet.v[0];
+  float u = f * s.dot(h);
+  if (u < 0.0 || u > 1.0)
+    return res;
+  Vector q = s.cross(edge1);
+  float v = f * ray.direction.dot(q);
+  if (v < 0.0 || u + v > 1.0)
+    return res;
+  float tt = f * edge2.dot(q);
+  res.intersects = (tt > E);
+  return res;
+}
+
+Vector Tracer::intersectsRectangle(Vector up, Vector down, Ray &ray)
+{
+}
 
 template <typename T>
 void log(T content)
 {
   cout << content << endl;
+}
+
+template <typename T>
+void log(T content[])
+{
+  for (int i = 0; i < sizeof(T); i++)
+  {
+    cout << content[i] << " ";
+  }
+  cout << endl;
 }
 
 template <class T>
@@ -115,4 +230,79 @@ vector<T> strsplit(string input, char delimiter)
   }
   res.push_back(static_cast<T>(input.substr(0, pos)));
   return res;
+}
+
+template <typename T>
+bool sign(T value)
+{
+  return (value > 0) - (value < 0);
+}
+
+void Tracer::Render(const char *input_file, string output_file)
+{
+  vector<Vertex> vertices;
+  vector<Facet> faces;
+  parseOBJ(input_file, vertices, faces);
+  ofstream out(output_file, ios::binary);
+
+  // file header
+  unsigned short width = 512;
+  unsigned short height = 512;
+  unsigned short colorBit = 3;
+  unsigned int size = width * height * colorBit;
+  unsigned int offset = 27;
+  unsigned int shit = 12;
+  unsigned short filehead[4] = {width, height, 1, (unsigned short)(colorBit * 8)};
+
+  out << "BM";
+  out.write((char *)&size, sizeof(size));
+  out.seekp(4, ios::cur);
+  out.write((char *)&offset, sizeof(offset));
+  out.write((char *)&shit, sizeof(shit));
+  out.write((char *)&filehead, 12);
+  out.seekp(offset, ios::beg);
+
+  Vector plane_origin = Vector(main_camera.direction.normalize() + main_camera.position);
+
+  char *buf = new char[size];
+
+  for (int y = 0; y < main_camera.resY; y++)
+  {
+    for (int x = 0; x < main_camera.resX; x++)
+    {
+      float
+          xNorm = (x - main_camera.resX / 2) / (float)main_camera.resX,
+          yNorm = (y - main_camera.resY / 2) / (float)main_camera.resY,
+          dist_to_plane = (main_camera.position - plane_origin).length(),
+          FOVrad = main_camera.FOV / 180.f * M_PI,
+          realH = (float)(dist_to_plane * tanf(FOVrad));
+      Vector
+          position_on_plane = Vector(xNorm * realH / 2, yNorm * realH / 2, 0) + plane_origin,
+          rayDir = position_on_plane - main_camera.position;
+
+      Ray ray = Ray(main_camera.position, rayDir);
+      int i = 0;
+      for (; i < faces.size(); i++)
+      {
+        Vector v = intersectsTriangle(faces[i], ray);
+        if (v)
+        {
+          float dist = (v - main_camera.position).length();
+          *(buf + (y * height + x) * colorBit + 1) = (char)(dist * 0xFF);
+          break;
+        }
+      }
+      if (i >= faces.size())
+      {
+        *(buf + (y * height + x) * colorBit) = (char)0x0F;
+      }
+    }
+  }
+  out.write(buf, sizeof(buf) * 512 * 512);
+  out.close();
+}
+void Tracer::Render(const char *input_file, string output_file, Camera &camera)
+{
+  main_camera = camera;
+  Render(input_file, output_file);
 }
--- src/utils/tracer/Tracer.h
+++ src/utils/tracer/Tracer.h
@@ -3,6 +3,7 @@
 #include <vector>
 #include <set>
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -11,47 +12,89 @@ struct Vertex;
 
 struct Vector
 {
-    Vector();
-    Vector(float X, float Y, float Z);
-    Vector(const float coordinates[3]);
-    Vector(vector<float> vector);
-    float x, y, z;
+	Vector();
+	Vector(float X, float Y, float Z);
+	Vector(const float coordinates[3]);
+	Vector(vector<float> vector);
+	float x, y, z;
+
+	// operators
+	operator bool();
+	Vector operator+(Vector &v);
+	Vector operator-(Vector &v);
+	Vector operator*(float num);
+	bool operator==(Vector &v);
+	float dot(Vector &v);
+	Vector normalize();
+	float length();
+	Vector cross(Vector &v);
+
+	void print();
+	bool intersects = false;
 };
 
 struct Vertex
 {
-    Vertex();
-    Vertex(Vector vec);
-    Vector pt;
-    vector<Facet> facets;
+	Vertex();
+	Vertex(Vector vec);
+	Vector pt;
+	vector<Facet> facets;
 };
 
 struct Facet
 {
-    Facet(Vector v1, Vector v2, Vector v3);
-    Vector v[3];
+	Facet(Vector &v1, Vector &v2, Vector &v3);
+	Vector v[3];
+	Vector n;
 };
 
 struct Ray
 {
-    Vector pt, dir; // dir is local vector
+	Ray(Vector _origin, Vector _direction);
+	Vector origin, direction; // dir is local vector
 };
 
-template <typename T> void log(T content);
+struct Camera
+{
+	Vector
+		position = Vector(10, 10, 10),
+		direction = Vector(-1, -1, -1);
+	// normal = Vector(0.5, 1, 0);
+	int
+		resX = 512,
+		resY = 512;
+	float
+		FOV = 60,	// degrees
+		FOVmm = 200; // millimeters
+};
 
-template <typename T> vector<T> strsplit(string input, char delimiter);
+template <typename T>
+void log(T content);
+template <typename T>
+void log(T content[]);
+
+template <typename T>
+vector<T> strsplit(string input, char delimiter);
+
+template <typename T>
+bool sign(T value);
 
 class Tracer
 {
   public:
-    void parseOBJ(const char *fname, vector<Vertex> &vertices, vector<Facet> &faces);
+	void parseOBJ(const char *fname, vector<Vertex> &vertices, vector<Facet> &faces);
 
-    //<Point of intersection, Normal>
-    pair<Vector, Ray> intersectsTriangle(Facet facet, Ray ray);
+	//<Point of intersection, Normal>
+	Vector intersectsTriangle(Facet &facet, Ray &ray);
+	Vector intersectsRectangle(Vector up, Vector down, Ray &ray);
 
-    pair<Vector, Ray> intersectsRectangle(Vector up, Vector down, Ray ray);
+	void Render(const char *input_file, string output_file);
+	void Render(const char *input_file, string output_file, Camera &camera);
+
+	// pair<Vector, Ray> intersectsTriangle(Facet facet, Ray ray); // nope, its not useful
+	// pair<Vector, Ray> intersectsRectangle(Vector up, Vector down, Ray ray);
 
   private:
-    Vector local_to_world(Vector *child, Vector *parent);
-    Vector world_to_local(Vector *child, Vector *parent); // child & parent should be in local coords
+	Vector local_to_world(Vector *child, Vector *parent);
+	Vector world_to_local(Vector *child, Vector *parent); // child & parent should be in local coords
 };
